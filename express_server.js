b465785e1290b86275c081cfd6d3a9913b3aa136
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-20 23:53:55 -0800

    ejs views added

--- express_server.js
+++ express_server.js
@@ -21,6 +21,12 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+})
