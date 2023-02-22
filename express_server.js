bee79d079775c675e31fadd8022312b262cab729
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-21 17:32:13 -0800

    using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,39 +1,16 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
+const cookieParser = require('cookie-parser');
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-});
-
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
-app.get('/urls/:id', (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render('urls_show', templateVars);
-});
-
-app.post('/urls/:id', (req, res) => {
-  const id = req.params.id;
-  urlDatabase[id] = req.body.newUrl;
-  res.redirect('/urls');
-});
 
 function generateRandomString() {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -46,11 +23,52 @@ function generateRandomString() {
   return randomString;
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
+  res.render('urls_index', templateVars);
+});
+
 app.post('/urls', (req, res) => {
   let newId = generateRandomString();
   urlDatabase[newId] = req.body.longURL;
   
   res.redirect(`/urls/${newId}`);
+});
+
+app.get('/urls/new', (req, res) => {
+  const templateVars = {
+    username: req.cookies['username']
+  };
+  res.render('urls_new', templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies['username']
+  };
+  res.render('urls_show', templateVars);
+});
+
+app.post('/urls/:id', (req, res) => {
+  const id = req.params.id;
+  urlDatabase[id] = req.body.newUrl;
+  res.redirect('/urls');
+});
+
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
 });
 
 app.get('/u/:id', (req, res) => {
@@ -59,13 +77,8 @@ app.get('/u/:id', (req, res) => {
   res.redirect(longURL);
 });
 
-app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
-});
-
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
+  res.cookie('username', req.body.username);  
   res.redirect('/urls');
 });
 
